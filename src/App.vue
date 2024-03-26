32c3c53800f60e4529ab2d0b64893fee3fbf44d1
libevm <libevm@proton.me>
2024-03-26 15:55:30 +1100

    pls

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <pre><output>
+ETHERNET TERMINAL INTERFACE v8.8
+Decentralized Hackerhouse OS
+-- NODE 0x50ZUH4U5 --
+DATETIME: [07-05-2024 08:00:00+AEST] - [13-05-2024 12:00:00+AEST]
+LOCATION: COOLGARDIE, 2478, AUSTRALIA
+
+> WELCOME TO ETHEREUM COMMAND CENTER
+------------------------------------
+
+[1] <a href="https://vitalik.eth.limo/general/2023/12/28/cypherpunk.html">Cypherpunk</a>
+[2] <a href="https://vitalik.eth.limo/general/2024/01/30/cryptoai.html">AI</a>
+[3] <a href="https://www.youtube.com/watch?v=YrAlAHbgRCk">MEV</a>
+[4] <a href="https://www.youtube.com/watch?v=f4X841gDnwY">ZK / ZKML</a>
+[5] Exit Terminal
+
+SELECT OPTION: _
+------------------------------------
+
+> Retrieving Blocks...
+...Loading...Please Wait...
+
+[BLOCK: CYPHERPUNK]
+"Hash: 0xcyph...erp1, Message: Privacy is a right, Encrypt everything, E2E"
+
+[BLOCK: AI]
+"Hash: 0xai21...0045, Smart Contract: AI-based Price Prediction, Data: Encrypted"
+
+[BLOCK: MEV]
+"Hash: 0xmev5...8b3e, Extracted Value: 3.2 ETH"
+
+[BLOCK: ZK/ZKML]
+"Hash: 0xzkf0...1b2c, Proof: zk-SNARKs, Data Privacy: Enabled, Gas: Efficient"
+
+------------------------------------
+
+<a href="https://lu.ma/zbhzmq7x">[APPLY HERE]</a></output></pre>
+  </div>
+</template>
